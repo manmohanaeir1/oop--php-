0f39e436077b73d0b0c22262069beb0adae72f90
Manmohan Aeir <manmohanaeir001110@gmail.com>
2022-09-13 14:11:04 +0545

    oop in php

--- first.php
+++ first.php
@@ -1,1 +1,18 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>object oriented php</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+echo "Hello Manmohan";
+
+?>
