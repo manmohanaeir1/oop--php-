ea738306c7df3fc97c3bd6ff7017241026ac6a4a
Manmohan Aeir <manmohanaeir001110@gmail.com>
2022-09-13 16:54:05 +0545

    class(properties and methods) in php

--- class_and_method.php
+++ class_and_method.php
@@ -1,10 +1,30 @@
 <?php
-class User{
+class User
+{
 
-    public $username= 'Manmohan';
-    public $useremail= 'man@gmail.com';
+    public $username = 'Manmohan';
+    public $useremail = 'man@gmail.com';
+
+    public function addfriend()
+    {
+        echo "$this->username";
+        echo "Friend is added!!";
+    }
 }
 
 $userOne = new User();
-echo 'name :' . $userOne->username;
-?>
+$userTwo = new User();
+echo 'name :' . $userOne->username . '<br>';
+echo 'email :'  . $userOne->useremail . '<br>';
+echo 'add friend :' . $userOne->addfriend() . '<br>';
+ echo '<br>';
+$userTwo->username = 'Ram';
+$userTwo->useremail = 'ram@gamil.com';
+
+echo 'name :' . $userTwo->username . '<br>';
+echo 'email :'  . $userTwo->useremail . '<br>';
+echo 'add friend :' . $userTwo->addfriend() . '<br>';
+
+
+print_r(get_class_vars('User'));
+print_r(get_class_methods('User'));
